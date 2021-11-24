992f60ec804b2bdb072635a24f9f4d6d8714d968
Brooklyn-lab1 <javascriptnative@gmail.com>
2021-11-24 21:29:02 +0200

    add new corrected

--- #src/js/script.js
+++ #src/js/script.js
@@ -4,7 +4,6 @@
 
 jQuery(document).ready(function () {
    //----Format Webp---------
-
    function testWebP(callback) {
       let webP = new Image();
       webP.onload = webP.onerror = function () {
@@ -19,433 +18,124 @@ jQuery(document).ready(function () {
       }
    });
 
-   // ----------- Burger Menu ------------------
+   // const dateSpan = document.querySelector('#date');
+   let dateSpan = $('#date')
+   const monthsAll = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-   // let burger = document.querySelector('.burger'),
-   //    nav = document.querySelector('.header__nav'),
-   //    body = document.querySelector('body');
+   const date = new Date();
+   let day = date.getDate();
+   let year = date.getFullYear();
+   let months = date.getMonth();
 
-   // burger.addEventListener('click', () => {
-   //    burger.classList.toggle('active')
-   //    nav.classList.toggle('active')
-   //    body.classList.toggle('active')
-   // })
+   monthsAll.map((m, i) => (months === i) ? months = m : null)
 
-   let allMenu = $('.burger, .header__nav, body');
-   $('.burger').click(() => allMenu.toggleClass('active'))
+   const today = `${months} ${day}, ${year}`
 
-   // -------------  Smooth scroll link --------------------
+   // dateSpan.innerHTML = today;
+   dateSpan.html(today);
 
-   // let smoothLinks = document.querySelectorAll('a[href*="#"]');
+   const body = $('.survey__body');
 
-   // smoothLinks.forEach(elem => {
-   //    elem.addEventListener('click', event => {
-   //       event.preventDefault()
-
-   //       let id = elem.getAttribute('href')
-
-   //       document.querySelector(id).scrollIntoView({
-   //          behavior: 'smooth',
-   //          block: 'start'
-   //       })
-   //    })
-   // })
-
-   $('a[href*="#"]').on('click', function (e) {
-      e.preventDefault();
-      $('html, body').animate({
-         scrollTop: $($(this).attr('href')).offset().top,
-      }, 500,
-         'linear',
-      );
-   })
-
-   // -------------- Accord menu ------------------------
-
-   // let headerAccord = document.querySelectorAll('.faq__header');
-
-   // headerAccord.forEach(head => {
-   //    head.addEventListener('click', function () {
-   //       let activeWrapperItem = document.querySelectorAll('.faq__text-wrapper.active')
-
-   //       activeWrapperItem.forEach(item => {
-   //          item.classList.remove('active')
-   //       })
-
-   //       let parentItem = head.parentNode,
-   //          wrapperItem = parentItem.querySelector('.faq__text-wrapper');
-
-   //       wrapperItem.classList.toggle('active')
-   //    })
-   // })
-
-   $('.faq__header').on('click', function () {
-      $('.faq__text-wrapper.active').removeClass('active');
-      $(this).parent().children('.faq__text-wrapper').toggleClass('active');
-   })
-
-   // ------------- Form ---------------------------
-   //    let button = $('#button'),
-   //    backBatton = $('.contact__prev'),
-   //    content = $('.form__content'),
-   //    stepNum = $('#progresNumber'),
-   //    step = 1,
-
-   //    data = {
-   //       personalized: '',
-   //       name: '',
-   //       lastName: '',
-   //       number: '',
-   //       date: ''
-   //    };
-
-   // const step_1 = `
-   // <p class="form__title">
-   //    Request your personalized loan-proposal
-   // </p>
-
-   // <div class="form__wrapper">
-   //    <label for="radio1" class="form__radio-btn">
-   //       <input class="form__radio-input" type="radio" name="personalized" id="radio1" data-attribute='<5k'>
-   //       <span class="form__radio-text">Less than €5,000</span>
-   //    </label>
-   //    <label for="radio2" class="form__radio-btn">
-   //       <input class="form__radio-input" type="radio" name="personalized" id="radio2" data-attribute='>5k'>
-   //       <span class="form__radio-text">€5,000-€25,000</span>
-   //    </label>
-   //    <label for="radio3" class="form__radio-btn">
-   //       <input class="form__radio-input" type="radio" name="personalized" id="radio3" data-attribute='>25k'>
-   //       <span class="form__radio-text">More than €25,000</span>
-   //    </label>
-   // </div>   
-   // `
-   // const step_2 = `
-   // <p class="form__title">
-   // Fill the rows below to suits better loan-offer for you
-   // </p>
-
-   // <div class="form__wrapper">
-   //    <input type="text" name='name' class="form__input" placeholder="First name" pattern="[A-Za-zА-Яа-яЁё]">
-   //    <input type="text" name='lastName' class="form__input" placeholder="Last name">
-   // </div>
-   // `
-   // const step_3 = `
-   // <p></p>
-   // <div class="form__wrapper">
-   //    <input type="text" name='number' class="form__input" placeholder="ID-number" data-attribute='number'>
-   //    <input type="text" name='date' class="form__input" placeholder="Date of birth" data-attribute='birth'>
-   // </div>
-   // `
-   // const step_4 = `
-   // <p>The form has been sent, thanks!</p>
-   // `
-
-   // content.innerHTML = step_1;
-   // stepNum.innerHTML = step;
-
-   // // функция проверки на наличие checked у input type='radio'
-   // function radioChecked() {
-   //    // находим в форме все input type='radio'
-   //    let radioInputs = content.querySelectorAll('input[type="radio"]');
-   //    // проходим по массиву и отделяем каждый input
-   //    radioInputs.forEach(input => {
-   //       // вешаем слушатель события по клику на каждый input
-   //       input.addEventListener('click', function () {
-   //          // проверяем наличие attribute checked у input
-   //          if (input.checked) {
-   //             // обращаемся к объекту и перезаписываем его ключ значение
-   //             data.personalized = input.getAttribute('data-attribute')
-   //          }
-   //       })
-   //       // проверяем наличие attribute checked у input
-   //       if (input.checked) {
-   //          // обращаемся к объекту и перезаписываем его ключ значение
-   //          data.personalized = input.getAttribute('data-attribute')
-   //       }
-   //       // находим input с attr совпадающим с data.personalized
-   //       if (data.personalized === input.getAttribute('data-attribute')) {
-   //          // При совпадении даем этому input checked
-   //          input.setAttribute('checked', true)
-   //       }
-   //    })
-   // }
-
-
-   // function inputVal() {
-   //    let inputs = content.querySelectorAll('input[type="text"]');
-
-   //    inputs.forEach(input => {
-   //       switch (input.getAttribute('name')) {
-   //          case 'name':
-   //             data.name = input.value;
-   //             break;
-   //          case 'lastName':
-   //             data.lastName = input.value;
-   //             break;
-   //          case 'number':
-   //             data.number = input.value;
-   //             break;
-   //          case 'date':
-   //             data.date = input.value;
-   //             break;
-   //       }
-   //    })
-   // }
-
-   // // Счетчик вверх
-   // function countUp() {
-   //    step = step + 1;
-   //    stepNum.innerHTML = step;
-   // }
-   // // Счетчик вниз 
-   // function countDown() {
-   //    step = step - 1
-   //    stepNum.innerHTML = step;
-   // }
-
-   // function stepUp() {
-   //    // Если step равен 1 и data.personalized не пустой то ...
-   //    if (step === 1) {
-   //       radioChecked();
-   //       // Проверяем нажата ли radio button
-   //       if (data.personalized.length !== 0) {
-   //          content.innerHTML = step_2;
-   //          countUp();
-   //          backBatton.classList.toggle('disable')
-   //       }
-   //    } else if (step === 2) {
-   //       inputVal();
-   //       // Проверяем, что заполнены оба инпута
-   //       if (data.name.length !== 0 && data.lastName.length !== 0) {
-   //          content.innerHTML = step_3;
-   //          countUp();
-   //       }
-   //    } else if (step === 3) {
-   //       inputVal();
-   //       // Проверяем, что заполнены оба инпута
-   //       if (data.number.length !== 0 && data.date.length !== 0) {
-   //          submitForm();
-   //          content.innerHTML = step_4;
-   //       }
-   //    }
-   // }
-
-   // function stepDown() {
-   //    switch (step) {
-   //       case 2:
-   //          content.innerHTML = step_1;
-   //          countDown();
-   //          radioChecked();
-   //          backBatton.classList.toggle('disable');
-   //          break;
-   //       case 3:
-   //          content.innerHTML = step_2;
-   //          countDown();
-   //          inputVal();
-   //          break;
-   //    }
-   // }
-
-   // // При нажатии меняет степ на +1
-   // button.addEventListener('click', (e) => {
-   //    e.preventDefault();
-   //    stepUp()
-   //    console.log(data)
-   // })
-   // // При нажатии меняет степ на -1
-   // backBatton.addEventListener('click', () => {
-   //    stepDown()
-   //    console.log(data)
-   // })
-
-   // // отпраква формы 
-   // const submitForm = async () => {
-   //    try {
-   //       await $.ajax({
-   //          url: 'http://localhost:8000/posts',
-   //          type: 'POST',
-   //          data: {
-   //             personalized: data.personalized,
-   //             name: data.name,
-   //             lastName: data.lastName,
-   //             idNumber: data.number,
-   //             birthDate: data.date
-   //          }
-   //       })
-   //    } catch (error) {
-   //       console.error(error);
-   //    }
-   // }
-
-   // const submitForm = async () => {
-   //    await fetch('http://localhost:8000/posts', {
-   //       method: 'POST',
-   //       headers: {
-   //          'Content-Type': 'application/json;charset=utf-8'
-   //       },
-   //       body: JSON.stringify(data)
-   //    });
-   // }
-
-
-
-   let button = $('#button'),
-      backBatton = $('.contact__prev'),
-      content = $('.form__content'),
-      stepNum = $('#progresNumber'),
-      step = 1,
-
-      data = {
-         personalized: '',
-         name: '',
-         lastName: '',
-         number: '',
-         date: ''
-      };
+   data = {
+      email: '',
+      question_1: '',
+      question_2: '',
+   }
 
    const step_1 = `
-<p class="form__title">
-   Request your personalized loan-proposal
-</p>
+   <form action="" class="survey-form">
+      <div class="survey-form__wrapper">
+         <label for="radio1" class='survey-form__label'>
+            <input type="radio" name="radio1" id="radio1">
+            <span>Daily1</span>
+         </label>
+         <label for="radio2" class='survey-form__label'>
+            <input type="radio" name="radio1" id="radio2">
+            <span>Daily2</span>
+         </label>
+         <label for="radio3" class='survey-form__label'>
+            <input type="radio" name="radio1" id="radio3">
+            <span>Daily3</span>
+         </label>
+         <label for="radio4" class='survey-form__label'>
+            <input type="radio" name="radio1" id="radio4">
+            <span>Daily4</span>
+         </label>
+         <label for="radio5" class='survey-form__label'>
+            <input type="radio" name="radio1" id="radio5">
+            <span>Daily5</span>
+         </label>
+         <label for="radio6" class='survey-form__label'>
+            <input type="radio" name="radio1" id="radio6">
+            <span>Daily6</span>
+         </label>
+      </div>
+      <button class="survey-form" id='surveyButton'>Next</button>
+   </form>
+   `
 
-<div class="form__wrapper">
-   <label for="radio1" class="form__radio-btn">
-      <input class="form__radio-input" type="radio" name="personalized" id="radio1" data-attribute='<5k'>
-      <span class="form__radio-text">Less than €5,000</span>
-   </label>
-   <label for="radio2" class="form__radio-btn">
-      <input class="form__radio-input" type="radio" name="personalized" id="radio2" data-attribute='>5k'>
-      <span class="form__radio-text">€5,000-€25,000</span>
-   </label>
-   <label for="radio3" class="form__radio-btn">
-      <input class="form__radio-input" type="radio" name="personalized" id="radio3" data-attribute='>25k'>
-      <span class="form__radio-text">More than €25,000</span>
-   </label>
-</div>   
-`
    const step_2 = `
-<p class="form__title">
-Fill the rows below to suits better loan-offer for you
-</p>
-
-<div class="form__wrapper">
-   <input type="text" name='name' class="form__input" placeholder="First name" pattern="[A-Za-zА-Яа-яЁё]">
-   <input type="text" name='lastName' class="form__input" placeholder="Last name">
-</div>
+   <form action="" class="survey-form">
+      <div class="survey-form__wrapper">
+         <label for="radio1" class='survey-form__label'>
+            <input type="radio" name="radio1" id="radio1">
+            <span>Daily7</span>
+         </label>
+         <label for="radio2" class='survey-form__label'>
+            <input type="radio" name="radio1" id="radio2">
+            <span>Daily8</span>
+         </label>
+         <label for="radio3" class='survey-form__label'>
+            <input type="radio" name="radio1" id="radio3">
+            <span>Daily9</span>
+         </label>
+         <label for="radio4" class='survey-form__label'>
+            <input type="radio" name="radio1" id="radio4">
+            <span>Daily10</span>
+         </label>
+         <label for="radio5" class='survey-form__label'>
+            <input type="radio" name="radio1" id="radio5">
+            <span>Daily11</span>
+         </label>
+         <label for="radio6" class='survey-form__label'>
+            <input type="radio" name="radio1" id="radio6">
+            <span>Daily12</span>
+         </label>
+      </div>
+      <button class="survey-form" id='surveyButton'>Next</button>
+   </form>
 `
-   const step_3 = `
-<p></p>
-<div class="form__wrapper">
-   <input type="text" name='number' class="form__input" placeholder="ID-number" data-attribute='number'>
-   <input type="text" name='date' class="form__input" placeholder="Date of birth" data-attribute='birth'>
-</div>
-`
-   const step_4 = `
-<p>The form has been sent, thanks!</p>
-`
 
-   content.html(step_1);
-   stepNum.html(step);
+   const step_3 = ` <h1>Step 3</h1> `
 
-
-   function radioChecked() {
-      let allInputs = $('input[type="radio"]');
-
-      $.each(allInputs, function () {
-         $(this).on('click', function () {
-            data.personalized = $(this).attr('data-attribute')
-         })
-
-         if (data.personalized === $(this).attr('data-attribute')) {
-            $(this).attr('checked', true);
-         }
-      })
-   }
-   radioChecked()
-
-   function inputVal() {
-      let inputs = $('input[type="text"]');
-
-      $.each(inputs, function () {
-         let input = $(this)
-
-         switch (input.attr('name')) {
-            case 'name':
-               data.name = input.val();
-               break;
-            case 'lastName':
-               data.lastName = input.val();
-               break;
-            case 'number':
-               data.number = input.val();
-               break;
-            case 'date':
-               data.date = input.val();
-               break;
+   function checkedRadio(step, key, class1, class2) {
+      $.each($('input[type="radio"]'), function () {
+         if ($(this).is(':checked')) {
+            data[`${key}`] = $(this).next().text()
+            body.html(step);
+            body.removeClass(`${class1}`).addClass(`${class2}`)
          }
       })
    }
 
-      // Счетчик вверх
-      function countUp() {
-         step = step + 1;
-         stepNum.html(step);
-      }
-      // Счетчик вниз 
-      function countDown() {
-         step = step - 1
-         stepNum.html(step);
-      }
+   function changeStep() {
+      if (body.is('.previewStep')) {
+         data.email = $('input[name="email"]').val();
 
-      
-   function stepUp() {
-      if (step === 1) {
-         radioChecked();
-         if (data.personalized.length !== 0) {
-            content.html(step_2);
-            countUp();
-            backBatton.toggleClass('disable')
+         if (data.email.length !== 0) {
+            body.html(step_1);
+            body.removeClass('previewStep').addClass('firstStep')
          }
-      } else if (step === 2) {
-         inputVal();
-         if (data.name.length !== 0 && data.lastName.length !== 0) {
-            content.html(step_3);
-            countUp();
-         }
-      } else if (step === 3) {
-         inputVal();
-         if (data.number.length !== 0 && data.date.length !== 0) {
-            submitForm();
-            content.html(step_4);
-         }
+      } else if (body.is('.firstStep')) {
+         checkedRadio(step_2, 'question_1', 'firstStep', 'secondStep')
+      } else if (body.is('.secondStep')) {
+         checkedRadio(step_3, 'question_2', 'secondStep', 'thirdStep')
       }
    }
 
-   function stepDown() {
-      switch (step) {
-         case 2:
-            content.html(step_1);
-            countDown();
-            radioChecked();
-            backBatton.toggleClass('disable');
-            break;
-         case 3:
-            content.html(step_2);
-            countDown();
-            inputVal();
-            break;
-      }
-   }
-
-   button.on('click', (e) => {
+   body.on('click', 'button', function (e) {
       e.preventDefault();
-      stepUp()
-      console.log(data)
+      changeStep()
+      console.log(data);
    })
-
-   backBatton.on('click', () => {
-      stepDown()
-      console.log(data)
-   })
-
 });
